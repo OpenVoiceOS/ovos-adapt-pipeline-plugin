8350928a5546a6d4ec7722eb9f939f1d2839f6a9
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2026-03-03 01:39:44 +0000

    fix: default config value
    related PR: https://github.com/OpenVoiceOS/ovos-config/pull/264

--- ovos_adapt/opm.py
+++ ovos_adapt/opm.py
@@ -53,9 +53,9 @@ class AdaptPipeline(ConfidenceMatcherPipeline):
     """Intent service wrapping the Adapt intent Parser."""
 
     def __init__(self, bus: Optional[Union[MessageBusClient, FakeBus]] = None,
-                 config: Optional[Dict] = None):
-        core_config = Configuration()
-        config = config or core_config.get("context", {})  # legacy mycroft-core path
+                 config: Optional[Dict] = None):       
+        intent_config = Configuration().get('intents', {})
+        config = config or intent_config.get("ovos-adapt-pipeline-plugin") or intent_config.get("adapt") or dict()
         super().__init__(bus, config)
         self.lang = standardize_lang(core_config.get("lang", "en-US"))
         langs = core_config.get('secondary_langs') or []
